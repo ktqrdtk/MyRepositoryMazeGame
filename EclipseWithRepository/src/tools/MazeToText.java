f75eece2c77fa84bc93d89cc0b95dc72a983e896
KTQRDTK <KTQRDTK@CCSD-45JYBH2.ccsdut.net>
2018-03-14 14:46:10 -0600

    Tools

--- EclipseWithRepository/src/tools/MazeToText.java
+++ EclipseWithRepository/src/tools/MazeToText.java
@@ -35,8 +35,8 @@ public class MazeToText implements ActionListener
 	
 	public void createGui()
 	{
-		JFrame frame = new JFrame("Maze Maker!");
-		JPanel mainPanel = new JPanel(new BorderLayout());
+		frame = new JFrame("Maze Maker!");
+		mainPanel = new JPanel(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.pack();
@@ -50,17 +50,28 @@ public class MazeToText implements ActionListener
 		txtArea = new JTextArea("*Put Maze Here*");
 		testButton = new JButton("Test Out Maze");
 		getButton = new JButton("Get Maze \nMust Be Tested");
+		testButton.addActionListener(this);
+		getButton.addActionListener(this);
+		testButton.setActionCommand("testMaze");
+		getButton.setActionCommand("getMaze");
+		testButton.setEnabled(true);
+		getButton.setEnabled(true);
 		buttonPanel = new JPanel();
 		buttonPanel.add(testButton);
-		
+		buttonPanel.add(getButton);
+		mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+		mainPanel.add(txtArea, BorderLayout.CENTER);
+		frame.revalidate();
+		frame.repaint();
 	}
 	
 	public void actionPerformed(ActionEvent evt)
 	{
 		String command = evt.getActionCommand();
+		System.out.println(command);
 		switch(command)
 		{
-		case "testButton":
+		case "testMaze":
 			testMaze();
 			break;
 			
@@ -72,12 +83,35 @@ public class MazeToText implements ActionListener
 	
 	public void testMaze()
 	{
-		
+		String[] array = stringToArray(this.txtArea.getText());
+		System.out.println(array[0]);
+		System.out.println("Testing Maze");
 	}
 	
 	public void getMaze()
 	{
 		
+	}
+	
+	public static String[] stringToArray(String input)
+	{
+		String[] array = new String[12];
+		char curChar = input.charAt(0);
+		String curLine = "";
+		for(int i = 0; i < input.length(); i++)
+		{
+			curChar = input.charAt(i);
+			if(curChar != '\n')
+			{
+				curLine += curChar;
+			}
+			else
+			{
+				array[i] = curLine;
+				curLine = "";
+			}
+		}
+		return array;
 	}
 
 }
