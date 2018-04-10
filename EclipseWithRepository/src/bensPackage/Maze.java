e1d46ec66e1d70442c7bde3e86e57b74e8f100ff
KTQRDTK <KTQRDTK@CCSD-45JYBH2.ccsdut.net>
2018-04-10 14:08:09 -0600

    a

--- EclipseWithRepository/src/bensPackage/Maze.java
+++ EclipseWithRepository/src/bensPackage/Maze.java
@@ -68,22 +68,24 @@ public class Maze {
 	public String getString(int location, boolean hasSpaces)
 	{
 		String returnString = "";
-		for(int i = 0; i < this.getGrid(location).getGrid().length; i++)
+		char[][] thisGrid = this.getGrid(location).getGrid();
+		for(int i = 0; i < thisGrid.length; i++)
 		{
-			for(int j = 0; j < this.getGrid(location).getGrid()[i].length; j++)
+			for(int j = 0; j < thisGrid[i].length; j++)
 			{
-				returnString += this.getGrid(location).getGrid()[i][j];
-				if(hasSpaces && j < (this.getGrid(location).getGrid()[i].length - 1))
+				returnString += thisGrid[i][j];
+				if(hasSpaces && j < (thisGrid[i].length - 1))
 				{
 					returnString += " ";
 				}
 			}
-			if(i < (this.getGrid(location).getGrid().length - 1))
+			if(i < (thisGrid.length - 1))
 			{
 				returnString += "\n";
 			}
 		}
-		
+		System.out.println(location);
+		System.out.println(returnString);
 		return returnString;
 	}
 	
@@ -91,74 +93,6 @@ public class Maze {
 	{
 		return this.fontSize;
 	}
-	
-	/*public Grid getGrid(int location)
-	{
-		if(this.getSize() == 1)
-		{
-			return maze[0][0];
-		}
-		else if(this.getSize() == 2)
-		{
-			if(location == 0)
-			{
-				return maze[0][0];
-			}
-			else if(location == 1)
-			{
-				return maze[0][1];
-			}
-			else if(location == 2)
-			{
-				return maze[1][0];
-			}
-			else if(location == 3)
-			{
-				return maze[1][1];
-			}
-		}
-		else if(this.getSize() == 3)
-		{
-			if(location == 0)
-			{
-				return maze[0][0];
-			}
-			else if(location == 1)
-			{
-				return maze[0][1];
-			}
-			else if(location == 2)
-			{
-				return maze[0][2];
-			}
-			else if(location == 3)
-			{
-				return maze[1][0];
-			}
-			else if(location == 4)
-			{
-				return maze[1][1];
-			}
-			else if(location == 5)
-			{
-				return maze[1][2];
-			}
-			else if(location == 6)
-			{
-				return maze[2][0];
-			}
-			else if(location == 7)
-			{
-				return maze[2][1];
-			}
-			else if(location == 8)
-			{
-				return maze[2][2];
-			}
-		}
-		return null;
-		
-	}*/
 	
 	public Grid getGrid(int location)
 	{
